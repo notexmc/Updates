708e8a2f153140372c44e83d1afa32cc7229f29d
Doharbdjr <119146391+Doharbdjr@users.noreply.github.com>
2024-04-21 18:15:28 +0600

    Fixed config.mjs

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "Notex Updates",
   description: "A VitePress Site",
-  base: '/updates/'
+  base: '/updates/',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
