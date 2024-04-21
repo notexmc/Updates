fc0b717e7266c1a009cd369006ad6681a6036f8d
Doharbdjr <119146391+Doharbdjr@users.noreply.github.com>
2024-04-21 21:13:02 +0600

    Update config.mjs

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -16,7 +16,7 @@ export default defineConfig({
       {
         text: 'Changelogs',
         items: [
-          { text: 'Bedrock Release 1.0.0', link: '/b-2401a' },
+          { text: 'Bedrock 1.0.0', link: '/b-2401a' },
         ]
       }
     ],
